2a801fd0a9bd3f08d01dc8bed1c49b1750383a25
Paul Demets <paul.demets@hotmail.com>
2020-10-22 15:39:44 +0200

    init API

---
+++ db/db.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require('mongodb');
+
+
+async function listDatabases(client) {
+
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb://joe:doe@localhost:27017/masterclass_project";
+
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+module.exports = { main };
