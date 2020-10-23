d4270a7c48d1602bcf16ce780d6074f22961bf0f
Paul Demets <paul.demets@hotmail.com>
2020-10-23 16:55:35 +0200

    add error gestion

--- app.js
+++ app.js
@@ -4,52 +4,116 @@ const db = require("./db/db");
 const app = express();
 app.use(express.json());
 
-app.get('/api/getRestaurant/:id', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.getRestaurantById(req.params.id)
+
+app.get('/api/getRestaurant/:id', async (req, res, next) => {
+    return new Promise((resolve, reject) => {
+        resolve(db.getRestaurantById(req.params.id))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
 app.post('/api/newRestaurant', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.createRestaurant(req.body)
+    return new Promise((resolve, reject) => {
+        resolve(db.createRestaurant(req.body))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
 app.put('/api/restaurants/:restaurant_id', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.updateRestaurant(req.params.restaurant_id, req.body)
+    return new Promise((resolve, reject) => {
+        resolve(db.updateRestaurant(req.params.restaurant_id, req.body))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
 app.delete('/api/restaurants/:restaurant_id', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.deleteRestaurant(req.params.restaurant_id)
+    return new Promise((resolve, reject) => {
+        resolve(db.deleteRestaurant(req.params.restaurant_id))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
 app.get('/api/restaurants_price_average', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.getAverageRestaurantsPrice()
+    return new Promise((resolve, reject) => {
+        resolve(db.getAverageRestaurantsPrice())
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
 app.get('/api/restaurants', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.findRestaurantsWithLocation(req.query.long_coordinates, req.query.lat_coordinates, req.query.max_distance)
+    return new Promise((resolve, reject) => {
+        resolve(db.findRestaurantsWithLocation(req.query.long_coordinates, req.query.lat_coordinates, req.query.max_distance))
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
 app.get('/api/restaurants/average_rating', async (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        response: await db.getRestaurantRating()
+    return new Promise((resolve, reject) => {
+        resolve(db.getRestaurantRating())
+    }).then((state) => {
+        res.status(200).send({
+            success: 'true',
+            response: state
+        })
+    }).catch((error) => {
+        res.status(500).send({
+            success: 'false',
+            response: error.toString()
+        })
     })
 });
 
--- db/db.js
+++ db/db.js
@@ -15,102 +15,75 @@ async function listDatabases(client) {
 };
 
 async function getRestaurantById(restaurant_id) {
-    try {
-        result = await client.db("masterclass_project").collection("restaurants")
-            .findOne({ _id: ObjectId(restaurant_id) });
+    result = await client.db("masterclass_project").collection("restaurants")
+        .findOne({ _id: ObjectId(restaurant_id) });
 
-        if (result) {
-            return result;
-        } else {
-            return 'Not finding data...';
-        }
-    }
-    catch (e) {
-        return new Error(e);
+    if (result) {
+        return result;
+    } else {
+        return 'Not finding data...';
     }
 }
 
 async function createRestaurant(body) {
-    try {
-        result = await client.db("masterclass_project").collection("restaurants")
-            .insertOne(
-                { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates }
-            );
+    result = await client.db("masterclass_project").collection("restaurants")
+        .insertOne(
+            { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates }
+        );
 
-        if (result) {
-            return `Insert new restaurant #${result.insertedId}`;
-        } else {
-            return 'error...';
-        }
-    }
-    catch (e) {
-        return new Error(e);
+    if (result) {
+        return `Insert new restaurant #${result.insertedId}`;
+    } else {
+        return 'error...';
     }
 }
 
 async function updateRestaurant(restaurant_id, body) {
-    try {
-        result = await client.db("masterclass_project").collection("restaurants")
-            .updateOne(
-                { _id: ObjectId(restaurant_id) },
-                { $set: { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates } }
-            );
-        if (result) {
-            return `Edited restaurant #${restaurant_id}`;
-        } else {
-            return 'error...';
-        }
-    }
-    catch (e) {
-        return new Error(e);
+    result = await client.db("masterclass_project").collection("restaurants")
+        .updateOne(
+            { _id: ObjectId(restaurant_id) },
+            { $set: { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates } }
+        );
+    if (result) {
+        return `Edited restaurant #${restaurant_id}`;
+    } else {
+        return 'error...';
     }
 }
 
 async function deleteRestaurant(restaurant_id) {
-    try {
+    result = await client.db("masterclass_project").collection("restaurants")
+        .deleteOne({ _id: ObjectId(restaurant_id) });
 
-        result = await client.db("masterclass_project").collection("restaurants")
-            .deleteOne({ _id: ObjectId(restaurant_id) });
-
-        if (result) {
-            return `Deleted restaurant #${restaurant_id} successful.`;
-        } else {
-            return 'error...';
-        }
-    }
-    catch (e) {
-        return new Error(e);
+    if (result) {
+        return `Deleted restaurant #${restaurant_id} successful.`;
+    } else {
+        return 'error...';
     }
 }
 
 async function findRestaurantsWithLocation(long_coordinates, lat_coordinates, max_distance) {
-    try {
-        console.log(long_coordinates);
-        await client.db("masterclass_project").collection("restaurants").createIndex({ "location": "2dsphere" });
+    await client.db("masterclass_project").collection("restaurants").createIndex({ "location": "2dsphere" });
 
-        result = await client.db("masterclass_project").collection("restaurants")
-            .find({
-                location: {
-                    $near: {
-                        $geometry: {
-                            type: "Point",
-                            coordinates: [parseFloat(long_coordinates), parseFloat(lat_coordinates)]
-                        },
-                        $maxDistance: parseInt(max_distance),
-                        $minDistance: 0
-                    }
+    result = await client.db("masterclass_project").collection("restaurants")
+        .find({
+            location: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [parseFloat(long_coordinates), parseFloat(lat_coordinates)]
+                    },
+                    $maxDistance: parseInt(max_distance ? max_distance : 5000),
+                    $minDistance: 0
                 }
-            }).toArray();
+            }
+        }).toArray();
 
 
-        if (result) {
-            return result;
-        } else {
-            return 'error...';
-        }
-    }
-    catch (e) {
-        return new Error(e);
+    if (result) {
+        return result;
+    } else {
+        return 'error...';
     }
 }
 
