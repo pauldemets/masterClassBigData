6604b3f0e7858cb5c5c4b3d3d01f0da7aba84dfd
Paul Demets <paul.demets@hotmail.com>
2020-10-23 16:23:15 +0200

    add requests

--- app.js
+++ app.js
@@ -2,14 +2,50 @@ const express = require("express");
 const db = require("./db/db");
 
 const app = express();
+app.use(express.json());
 
-app.get('/api/v1/todos', (req, res) => {
+app.get('/api/getRestaurant/:id', async (req, res) => {
     res.status(200).send({
         success: 'true',
-        message: 'todos retrieved successfully',
-        todos: db
+        response: await db.getRestaurantById(req.params.id)
     })
 });
+
+app.post('/api/newRestaurant', async (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        response: await db.createRestaurant(req.body)
+    })
+});
+
+app.put('/api/restaurants/:restaurant_id', async (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        response: await db.updateRestaurant(req.params.restaurant_id, req.body)
+    })
+});
+
+app.delete('/api/restaurants/:restaurant_id', async (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        response: await db.deleteRestaurant(req.params.restaurant_id)
+    })
+});
+
+app.get('/api/restaurants_price_average', async (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        response: await db.getAverageRestaurantsPrice()
+    })
+});
+
+app.get('/api/restaurants/average_rating', async (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        response: await db.getRestaurantRating()
+    })
+});
+
 
 const PORT = 5000;
 
--- db/db.js
+++ db/db.js
@@ -1,37 +1,148 @@
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
+
+const uri = "mongodb://joe:doe@localhost:27017/masterclass_project";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 
 async function listDatabases(client) {
-
     databasesList = await client.db().admin().listDatabases();
 
     console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    databasesList.databases.forEach(db =>
+        console.log(` - ${db.name}`),
+    );
 };
 
-async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb://joe:doe@localhost:27017/masterclass_project";
-
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-
+async function getRestaurantById(restaurant_id) {
     try {
+        result = await client.db("masterclass_project").collection("restaurants")
+            .findOne({ _id: ObjectId(restaurant_id) });
+
+        if (result) {
+            return result;
+        } else {
+            return 'Not finding data...';
+        }
+    }
+    catch (e) {
+        return new Error(e);
+    }
+}
+
+async function createRestaurant(body) {
+    try {
+        result = await client.db("masterclass_project").collection("restaurants")
+            .insertOne(
+                { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates }
+            );
+
+        if (result) {
+            return `Insert new restaurant #${result.insertedId}`;
+        } else {
+            return 'error...';
+        }
+    }
+    catch (e) {
+        return new Error(e);
+    }
+}
+
+async function updateRestaurant(restaurant_id, body) {
+    try {
+        result = await client.db("masterclass_project").collection("restaurants")
+            .updateOne(
+                { _id: ObjectId(restaurant_id) },
+                { $set: { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates } }
+            );
+        if (result) {
+            return `Edited restaurant #${restaurant_id}`;
+        } else {
+            return 'error...';
+        }
+    }
+    catch (e) {
+        return new Error(e);
+    }
+}
+
+async function deleteRestaurant(restaurant_id) {
+    try {
+
+        result = await client.db("masterclass_project").collection("restaurants")
+            .deleteOne({ _id: ObjectId(restaurant_id) });
+
+        if (result) {
+            return `Deleted restaurant #${restaurant_id} successful.`;
+        } else {
+            return 'error...';
+        }
+    }
+    catch (e) {
+        return new Error(e);
+    }
+}
+
+async function getProximityRestaurant() {
+
+}
+
+async function getAverageRestaurantsPrice() {
+    try {
+        result = await client.db("masterclass_project").collection("restaurants")
+            .aggregate(
+                [
+                    {
+                        $group:
+                            { _id: "", avg_price: { $avg: "$price" } }
+                    }
+                ]
+            ).toArray();
+
+        if (result) {
+            return `Average price for all the restaurants is ${result[0].avg_price}`;
+        } else {
+            return 'error...';
+        }
+    } catch (e) {
+        return new Error(e);
+    }
+}
+
+async function getRestaurantRating() {
+    try {
+        result = await client.db("masterclass_project").collection("restaurants")
+            .aggregate(
+                [{ $project: { _id: "$_id", name: "$name", avg_stars: { $avg: "$reviews" } } }]
+            ).toArray();
+
+        if (result) {
+            return result;
+        } else {
+            return 'error...';
+        }
+    }
+    catch (e) {
+        return new Error(e);
+    }
+}
+
+
+async function main() {
+    try {
+        console.log('Connection to DB...');
         // Connect to the MongoDB cluster
         await client.connect();
-
         // Make the appropriate DB calls
         await listDatabases(client);
-
     } catch (e) {
         console.error(e);
     } finally {
-        await client.close();
+        //   await client.close();
     }
 }
 
 main().catch(console.error);
 
-module.exports = { main };
+
+module.exports = { getRestaurantById, createRestaurant, updateRestaurant, deleteRestaurant, getRestaurantRating, getAverageRestaurantsPrice };
